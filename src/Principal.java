32a8cbfff4f536e858768cd39143aec2fe393c0b
Solutionbto <Solutionbto@Solutionbto-PC>
2014-05-03 14:15:10 -0300

    sending one or more attachments

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,7 @@
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.mail.EmailAttachment;
 import org.apache.commons.mail.HtmlEmail;
@@ -12,56 +14,72 @@ import org.apache.commons.mail.HtmlEmail;
  * criado em: 07/04/2014
  */
 public class Principal {
-	
+
 	public static void main(String[] args) {		
 		try {
-			 //object of attachment
-			 EmailAttachment attachment = new EmailAttachment(); 
-			 //set file local, convert in URL
-			 attachment.setURL(new File("./Commons Email - Examples.pdf").toURI().toURL());
-			 attachment.setDisposition(EmailAttachment.ATTACHMENT); 
-
-			 // Create the email message
-			  HtmlEmail email = new HtmlEmail();
-			  email.attach(attachment);
-			  //show log
-			  email.setDebug(true);
-			  //set port
-			  email.setSmtpPort(587);  
-			  //check this rules for security
-			  email.setStartTLSEnabled(true);
-			  //email.setSSLCheckServerIdentity(true);
+			// Create the email message
+			HtmlEmail email = new HtmlEmail();
 			
-			  email.setHostName("smtp.live.com");//server SMTP for Hotmail, alter other
-			  email.setAuthentication("yourEmail@hotmail.com", "yourPassword");
-			  email.setFrom("yourEmail@hotmail.com", "yourName");
-			  
-			  //destiny's
-			  email.addTo("yourDestinyEmail1", "yourDestinyName1");
-			  email.addTo("yourDestinyEmail2", "yourDestinyName2");
-			  email.addTo("yourDestinyEmail3", "yourDestinyName3");
-			  
-			  email.setSubject("Test email html with image and file attachment");
-			  
-			  //put the format to recognize the image,
-			  //the reason for not accepting all image formats
-			  String cid = email.embed(new File("./Apache_logo.gif"));
-			  
-			  //html text
-			  String txtHtml=
-					  "<html>"
-							  + "Hello,<br><br>"
-							  + "This one test for send email with image,<br>"
-							  + "thanks for cooperate.<br><br>"
-							  + "<a href=\"http://solutionbto.wordpress.com/\">"
-							  + "<img src=\"cid:"+cid+"\">"
-							  + "</a>"
-							  + "</html>";
-			  
-			  // set the html message
-			  email.setHtmlMsg(txtHtml);
-			  // send the email
-			  email.send();
+			File file1=new File("./Commons Email - Examples.pdf");
+			List<File> files=new ArrayList<File>();
+			files.add(file1);
+			
+			if(files.size() > 0){
+				//sending one or more attachments
+				EmailAttachment attachment = new EmailAttachment(); 
+				attachment.setDisposition(EmailAttachment.ATTACHMENT);
+
+				//for each in files
+				for(File fileIn:files){
+					if(fileIn != null && fileIn.isFile()){
+						int indexOfExtension=fileIn.getName().indexOf(".");
+						indexOfExtension=indexOfExtension == -1 ? 0 :indexOfExtension;
+						attachment.setDescription(fileIn.getName().substring(indexOfExtension));//extenção
+						attachment.setName(fileIn.getName());	  
+						attachment.setURL(fileIn.toURI().toURL());//set file local, convert in URL
+						email.attach(attachment);
+					}
+				}
+			}
+			
+			//show log
+			email.setDebug(true);
+			//set port
+			email.setSmtpPort(587);  
+			//check this rules for security
+			email.setStartTLSEnabled(true);
+			//email.setSSLCheckServerIdentity(true);
+
+			email.setHostName("smtp.live.com");//server SMTP for Hotmail, alter other
+			email.setAuthentication("yourEmail@hotmail.com", "yourPassword");
+			email.setFrom("yourEmail@hotmail.com", "yourName");
+
+			//destiny's
+			email.addTo("yourDestinyEmail1", "yourDestinyName1");
+			email.addTo("yourDestinyEmail2", "yourDestinyName2");
+			email.addTo("yourDestinyEmail3", "yourDestinyName3");
+
+			email.setSubject("Test email html with image and file attachment");
+
+			//put the format to recognize the image,
+			//the reason for not accepting all image formats
+			String cid = email.embed(new File("./Apache_logo.gif"));
+
+			//html text
+			String txtHtml=
+					"<html>"
+							+ "Hello,<br><br>"
+							+ "This one test for send email with image,<br>"
+							+ "thanks for cooperate.<br><br>"
+							+ "<a href=\"http://solutionbto.wordpress.com/\">"
+							+ "<img src=\"cid:"+cid+"\">"
+							+ "</a>"
+							+ "</html>";
+
+			// set the html message
+			email.setHtmlMsg(txtHtml);
+			// send the email
+			email.send();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
